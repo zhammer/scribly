777c2e39d3485d693e16973ad395ca6c60793ec7
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-11-09 09:01:41 -0500

    Quicker start to writing a story (#4)
    Can worry about getting collaborators after

--- scribly/server.py
+++ scribly/server.py
@@ -2,6 +2,7 @@ import base64
 import binascii
 import logging
 import random
+from typing import Tuple
 
 from starlette.applications import Starlette
 from starlette.authentication import (
@@ -20,20 +21,45 @@ logger = logging.getLogger(__name__)
 
 
 USERS = ("zach.the.hammer@gmail.com:password", "gsnussbaum@gmail.com:password")
-TITLES = (
-    "iris in the evening",
-    "alice strongbow",
-    "the man ghost and a very living dog",
-    "will you walk into my wavetrap?",
-    "alas, the island shrinks!",
-    "chemicals are making wood invisible",
-    "obama's baby bug",
-    "all the windows are fogging in new york",
-)
+
+STORY_STARTERS = [
+    (
+        "iris in the evening",
+        "Years of gardening had taken their toll. Without legs to walk on, with his family gone, all he had was his iris, and how it glowed as the evening sky came out, below the moonlight and the stars.",
+    ),
+    (
+        "alice strongbow",
+        "Alice strongbow knew what was to come as she laid her fathers to rest, hand in hand, as they drifted out to sea. She moved the tip of her arrow, smelling of gasoline, closer to the flame.",
+    ),
+    (
+        "the man ghost and a very living dog",
+        "'Here boy, here!' It seemed that the stories were true; dogs really *could* see ghosts. His pup turned around and ran right at him, and through him, and he was happy to know that at least for now, he had a friend.",
+    ),
+    (
+        "will you walk into my wavetrap?",
+        "Will you walk into my wavetrap? said the spiter to the shy.\n\nIf we each could always do all we ever did.",
+    ),
+    (
+        "alas, the island shrinks!",
+        "A manuscript found in the wreckage of an ancient ship tells the strange, and seemingly unbelievable, story of a small brigade of pirates cursed to only know the sea. It begins as such:",
+    ),
+    (
+        "chemicals are making wood invisible",
+        "What a strange sight to see the redwood forest, all clear, with light from the sun bending and warping as it passed through the leaves, spraying prismatic colors onto the brush and the water. Things on earth had certainly changed.",
+    ),
+    (
+        "obama's baby bug",
+        "As nights grew colder, and days grew shorter, at 1600 Penn -- as his daughters were off to school and his wife touring the country in promotion of better food -- Obama only took solace in visiting the small blue bug he had found in the back corner of an old, unused closet.",
+    ),
+    (
+        "all the windows are fogging in new york",
+        "It's december\nIt's getting colder\nAll the windows are fogging in new york.\n\nThe neighbors windows\nLook like icy boulders\nAnd yet I know inside it's awfully warm.",
+    ),
+]
 
 
-def random_title() -> str:
-    return random.choice(TITLES)
+def random_story_starter() -> Tuple[str, str]:
+    return random.choice(STORY_STARTERS)
 
 
 class BasicAuthBackend(AuthenticationBackend):
@@ -96,8 +122,14 @@ async def new_story(request):
     if not request.user.is_authenticated:
         return RedirectResponse("/")
 
+    random_title_suggestion, random_intro_suggestion = random_story_starter()
     return templates.TemplateResponse(
-        "newstory.html", {"request": request, "random_title_suggestion": random_title()}
+        "newstory.html",
+        {
+            "request": request,
+            "random_title_suggestion": random_title_suggestion,
+            "random_intro_suggestion": random_intro_suggestion,
+        },
     )
 
 
@@ -110,4 +142,3 @@ async def new_story_submit(request):
     logger.info("received new story submission: %s", form)
 
     return Response(status_code=200)
-
