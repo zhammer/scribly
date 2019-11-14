e5f8e8d93a437e9cb8c8f136f95b21a7250660ae
Zach Hammer <zachary_hammer@alumni.brown.edu>
2019-11-13 21:37:21 -0500

    Add story turns (#11)
    * Add pass turn use case
    
    * Add write turn use case
    
    * Add take turn finish
    
    * Add use case for turn write and finish
    
    * Hook up turn use cases to server
    
    * Use same template for in progress & finished stories
    
    * Small update to just pass story to other template
    
    * Don't shuffle cowriters
    
    It's going to make testing too hard
    
    * Add initial StoryTurn tests
    
    * Add one big test for simulating people writing a story

--- scribly/use_scribly.py
+++ scribly/use_scribly.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Sequence
 
 from scribly import policies
-from scribly.definitions import Context, Story, User
+from scribly.definitions import Context, Story, TurnAction, User
 from scribly.util import shuffle
 
 
@@ -38,6 +38,47 @@ class Scribly:
 
             policies.require_valid_cowriters(cowriters, cowriter_usernames)
 
-            full_cowriters = [user] + list(shuffle(cowriters))
+            full_cowriters = [user] + list(cowriters)
 
             return await self.context.database.add_cowriters(story, full_cowriters)
+
+    async def take_turn_pass(self, user: User, story_id: int) -> Story:
+        async with self.context.database.transaction():
+            story = await self.context.database.fetch_story(story_id, for_update=True)
+
+            policies.require_user_can_take_turn_pass(user, story)
+
+            return await self.context.database.add_turn_pass(user, story)
+
+    async def take_turn_write(
+        self, user: User, story_id: int, text_written: str
+    ) -> Story:
+        async with self.context.database.transaction():
+            story = await self.context.database.fetch_story(story_id, for_update=True)
+
+            policies.require_user_can_take_turn_write(user, story, text_written)
+
+            return await self.context.database.add_turn_write(user, story, text_written)
+
+    async def take_turn_finish(self, user: User, story_id: int) -> Story:
+        async with self.context.database.transaction():
+            story = await self.context.database.fetch_story(story_id, for_update=True)
+
+            policies.require_user_can_take_turn_finish(user, story)
+
+            return await self.context.database.add_turn_finish(user, story)
+
+    async def take_turn_write_and_finish(
+        self, user: User, story_id: int, text_written: str
+    ) -> Story:
+        async with self.context.database.transaction():
+            story = await self.context.database.fetch_story(story_id, for_update=True)
+
+            policies.require_user_can_take_turn_write_and_finish(
+                user, story, text_written
+            )
+
+            return await self.context.database.add_turn_write_and_finish(
+                user, story, text_written
+            )
+
