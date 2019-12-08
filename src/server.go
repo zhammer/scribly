c8a8055e5ddfbf79c78d205bb08e822072fbd3f7
zhammer <zachary_hammer@alumni.brown.edu>
2019-12-08 00:53:52 -0500

    Move go templates to their own directory

--- src/server.go
+++ src/server.go
@@ -4,12 +4,12 @@ import "github.com/gin-gonic/gin"
 import "net/http"
 
 func homepage(context *gin.Context) {
-	context.HTML(http.StatusOK, "index.tmpl", gin.H{})
+	context.HTML(http.StatusOK, "index.html", gin.H{})
 }
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLFiles("templates/index.tmpl")
+	router.LoadHTMLFiles("go_templates/index.html")
 	router.Static("/static", "./static")
 
 	router.GET("/", homepage)
