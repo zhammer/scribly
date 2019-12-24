b8f34ae873a88fa1405e579749cfb9877f060aba
zhammer <zachary_hammer@alumni.brown.edu>
2019-12-23 23:41:49 -0500

    Add more test cases for turn notification emails

--- scribly/emails_test.py
+++ scribly/emails_test.py
@@ -144,6 +144,165 @@ class TestTurnNotificationEmail:
         assert len(emails) == 1
         assert emails[0].to == zach.email
 
+    def test_sends_notifications_write_and_finish(self) -> None:
+        zach = make_user("zach", "verified")
+        gabe = make_user("gabe", "verified")
+        rakesh = make_user("rakesh", "verified")
+        cowriters = [zach, gabe, rakesh]
+
+        story = Story(
+            id=1,
+            title="A god walks into a bar",
+            state="done",
+            created_by=zach,
+            cowriters=cowriters,
+            turns=list(make_turns(["write", "write_and_finish"], cowriters)),
+        )
+
+        # when
+        emails = build_turn_email_notifications(story, turn_number=2)
+
+        # then
+        assert len(emails) == 2
+        zach_email = emails[0]
+        rakesh_email = emails[1]
+
+        assert zach_email.to == zach.email
+        assert rakesh_email.to == rakesh.email
+
+        expected_subject = f"{story.title} is done!"
+        assert zach_email.subject == expected_subject
+        assert rakesh_email.subject == expected_subject
+
+        expected_email_regex = re.compile(
+            (
+                r"<!DOCTYPE html>\s*<html lang=\"en\">\s*"
+                r"<head><style.*>.*</style></head>\s*"
+                r"<body.*>\s*"
+                r"<p>\s*<a href=\"http://127.0.0.1:8000/stories/1\">go to your story</a>\s*</p>\s*"
+                r"<p>\s*"
+                r"<span>\s*gabe wrote a section and finished the story!\s*</span>\s*"
+                r"</p>\s*"
+                r"<hr>\s*<em>"
+                f"{story.turns[-1].text_written}"
+                r"</em>\s*"
+                r"</body>\s*"
+                r"</html>$"
+            ),
+            re.DOTALL,
+        )
+        assert expected_email_regex.match(zach_email.body)
+        assert expected_email_regex.match(rakesh_email.body)
+
+    def test_sends_notifications_finish(self) -> None:
+        zach = make_user("zach", "verified")
+        gabe = make_user("gabe", "verified")
+        rakesh = make_user("rakesh", "verified")
+        cowriters = [zach, gabe, rakesh]
+
+        story = Story(
+            id=1,
+            title="A god walks into a bar",
+            state="done",
+            created_by=zach,
+            cowriters=cowriters,
+            turns=list(make_turns(["write", "finish"], cowriters)),
+        )
+
+        # when
+        emails = build_turn_email_notifications(story, turn_number=2)
+
+        # then
+        assert len(emails) == 2
+        zach_email = emails[0]
+        rakesh_email = emails[1]
+
+        assert zach_email.to == zach.email
+        assert rakesh_email.to == rakesh.email
+
+        expected_subject = f"{story.title} is done!"
+        assert zach_email.subject == expected_subject
+        assert rakesh_email.subject == expected_subject
+
+        expected_email_regex = re.compile(
+            (
+                r"<!DOCTYPE html>\s*<html lang=\"en\">\s*"
+                r"<head><style.*>.*</style></head>\s*"
+                r"<body.*>\s*"
+                r"<p>\s*<a href=\"http://127.0.0.1:8000/stories/1\">go to your story</a>\s*</p>\s*"
+                r"<p>\s*"
+                r"<span>\s*gabe finished the story!\s*</span>\s*"
+                r"</p>\s*"
+                r"</body>\s*"
+                r"</html>$"
+            ),
+            re.DOTALL,
+        )
+        assert expected_email_regex.match(zach_email.body)
+        assert expected_email_regex.match(rakesh_email.body)
+
+    def test_sends_notifications_pass(self) -> None:
+        zach = make_user("zach", "verified")
+        gabe = make_user("gabe", "verified")
+        rakesh = make_user("rakesh", "verified")
+        cowriters = [zach, gabe, rakesh]
+
+        story = Story(
+            id=1,
+            title="A god walks into a bar",
+            state="in_progress",
+            created_by=zach,
+            cowriters=cowriters,
+            turns=list(make_turns(["write", "pass"], cowriters)),
+        )
+
+        # when
+        emails = build_turn_email_notifications(story, turn_number=2)
+
+        # then
+        assert len(emails) == 2
+        zach_email = emails[0]
+        rakesh_email = emails[1]
+
+        assert zach_email.to == zach.email
+        assert rakesh_email.to == rakesh.email
+
+        assert rakesh_email.subject == f"It's your turn on {story.title}!"
+        assert zach_email.subject == f"gabe took their turn on {story.title}!"
+
+        expected_rakesh_email_regex = re.compile(
+            (
+                r"<!DOCTYPE html>\s*<html lang=\"en\">\s*"
+                r"<head><style.*>.*</style></head>\s*"
+                r"<body.*>\s*"
+                r"<p>\s*<a href=\"http://127.0.0.1:8000/stories/1\">go to your story</a>\s*</p>\s*"
+                r"<p>\s*"
+                r"<span>\s*gabe passed!\s*</span>\s*"
+                r"<span> it's your turn</span>\s*"
+                r"</p>\s*"
+                r"</body>\s*"
+                r"</html>$"
+            ),
+            re.DOTALL,
+        )
+        assert expected_rakesh_email_regex.match(rakesh_email.body)
+
+        expected_zach_email_regex = re.compile(
+            (
+                r"<!DOCTYPE html>\s*<html lang=\"en\">\s*"
+                r"<head><style.*>.*</style></head>\s*"
+                r"<body.*>\s*"
+                r"<p>\s*<a href=\"http://127.0.0.1:8000/stories/1\">go to your story</a>\s*</p>\s*"
+                r"<p>\s*"
+                r"<span>\s*gabe passed!\s*</span>\s*"
+                r"<span> it's rakesh's turn</span>\s*"
+                r"</p>\s*"
+                r"</body>\s*"
+            ),
+            re.DOTALL,
+        )
+        assert expected_zach_email_regex.match(zach_email.body)
+
 
 user_id = 1
 
