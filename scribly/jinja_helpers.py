6a2fe3cf71f073c694036322776fab01c9e31306
zhammer <zachary_hammer@alumni.brown.edu>
2020-02-01 13:28:00 -0500

    Revert "Really clean up blocks"
    This reverts commit 47b6c637d96697fd2b98d6b5fd3bd272cdc0feaa.
    
    not sure this is worth it and has some bugs

--- scribly/jinja_helpers.py
+++ scribly/jinja_helpers.py
@@ -6,7 +6,4 @@ from jinja2.ext import Extension
 
 class RemoveNewlines(Extension):
     def preprocess(self, source: str, name: str, filename: Optional[str] = None) -> str:
-        text_without_newlines = re.sub(r"(?<!>)\n\s+(?!<|\s)", " ", source)
-        return re.sub(
-            r"(?<=>)\n\s+(?!<|\s)|(?<!>)\n\s+(?=<)", "", text_without_newlines
-        )
+        return re.sub(r"(?<!>)\n\s+(?!<|\s)", " ", source)
