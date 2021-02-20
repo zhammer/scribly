1bebf9403bd930056907705a344d5c3a6163dcf5
Zach Hammer <zachary_hammer@alumni.brown.edu>
2021-02-20 17:38:40 -0500

    fix https redirect

--- cmd/site/middleware.go
+++ cmd/site/middleware.go
@@ -2,15 +2,12 @@ package main
 
 import (
 	"net/http"
-	"net/url"
 )
 
 func HerokuHTTPSMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("X-FORWARDED-PROTO") == "http" {
-			urlCopy, _ := url.Parse(r.URL.String())
-			urlCopy.Scheme = "https"
-			http.Redirect(w, r, urlCopy.String(), http.StatusTemporaryRedirect)
+			http.Redirect(w, r, "https://"+r.Host+r.URL.String(), http.StatusTemporaryRedirect)
 			return
 		}
 		h.ServeHTTP(w, r)
