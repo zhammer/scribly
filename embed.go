2f1808038369d8475d4d28bf6cda8da62d5ae0f6
Zach Hammer <zachary_hammer@alumni.brown.edu>
2021-02-20 20:25:54 -0500

    Use go embed (#95)

---
+++ embed.go
@@ -0,0 +1,17 @@
+package scribly
+
+import (
+	"embed"
+	"html/template"
+)
+
+//go:embed templates/*
+var templateFS embed.FS
+var EmailTemplates = template.Must(template.ParseFS(templateFS, "templates/email/*tmpl"))
+var WebTemplates = template.Must(template.ParseFS(templateFS, "templates/web/*tmpl"))
+
+//go:embed static/style.css
+var CSS string
+
+//go:embed static/*
+var StaticFS embed.FS
