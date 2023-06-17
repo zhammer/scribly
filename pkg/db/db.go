9b6058c0e003db67476366fb9142b5b776f171b7
Zach Hammer <zhammer@seatgeek.com>
2023-06-17 12:48:20 -0400

    update db

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,11 +1,11 @@
 package db
 
-import "github.com/go-pg/pg/v10/orm"
+import "github.com/uptrace/bun"
 
-type relationshipFunc func(*orm.Query) (*orm.Query, error)
+type relationshipFunc func(*bun.SelectQuery) (*bun.SelectQuery, error)
 
 func WithOrderBy(column string) relationshipFunc {
-	return func(q *orm.Query) (*orm.Query, error) {
+	return func(q *bun.SelectQuery) (*bun.SelectQuery, error) {
 		return q.Order(column), nil
 	}
 }
